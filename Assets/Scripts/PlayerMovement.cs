5278a0498e8173706fd05e40a68d44959d19b72a
Dylan <guidry.dylan.95@gmail.com>
2020-04-03 20:05:57 -0500

    GJ Start

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(0.1f, 10.0f)]
+    public float HorizontalSpeed;
+    [Range(0.1f, 10.0f)]
+    public float RotationSpeed;
+    [Range(0.0f, 10.0f)]
+    public float JumpForce;
+
+    private void Update()
+    {
+        
+    }
+}
