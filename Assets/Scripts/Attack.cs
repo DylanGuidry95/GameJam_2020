2c0c7cc76e67ac1f62418cf8aaa050211aa56eec
Dylan <guidry.dylan.95@gmail.com>
2020-04-03 23:11:01 -0500

    Setup for attacks

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public string AttackName;
+
+    public float Cooldown;
+
+    private float timer;
+
+    private bool CanActivate
+    {
+        get
+        {
+            if (timer >= Cooldown)
+            {
+                timer = 0;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    public virtual void Activate()
+    {
+        if (!CanActivate)
+            return;
+    }
+
+    public virtual void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [Header("Attacks")]
+    public List<Attack> AcquiredSpecials;
+    
+    [System.Serializable]
+    public class BoundAttack
+    {
+        public string ActivateButton;
+        public Attack Attack;
+    }
+
+    public BoundAttack SpecialAttackOne;
+    public BoundAttack SpecialAttackTwo;
+
+    [Header("Controls")]
+    public string BasicAttackButton = "Fire1";    
+
+    private void Update()
+    {        
+        if(Input.GetButtonDown(BasicAttackButton))
+        {
+            Debug.Log("I Attacked");
+        }
+
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+}
